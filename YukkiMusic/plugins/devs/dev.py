56f7e059f3d80ab9f2a6941cc831dcf72fd7ae9a
iamuput <yogaaasp73@gmail.com>
2024-01-18 15:21:11 +0700

    Update dev.py

--- YukkiMusic/plugins/devs/dev.py
+++ YukkiMusic/plugins/devs/dev.py
@@ -44,7 +44,7 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 
 @app.on_message(
-    filters.command("eval")
+    filters.command("ev")
     & SUDOERS
     & ~filters.forwarded
     & ~filters.via_bot
