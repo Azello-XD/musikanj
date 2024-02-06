d5a830c2be637cf090fc759419b57d409d4f36a8
IONBOT <113283570+ionmusic@users.noreply.github.com>
2024-02-07 05:33:36 +0700

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -38,7 +38,7 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
+                                and chat_id != -1001287188817
                                 and chat_id != -1001733534088
                                 and chat_id != -1001443281821
                             ):
